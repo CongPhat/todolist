3dc14f81cdab4518c80bb3651ba03ea62d8c355d
CongPhat <huynhcongphat2510@gmail.com>
2019-08-05 17:14:36 +0700

    Build Todolist with Reactjs

---
+++ precache-manifest.4d59b01c51312329671c4dabe32ca663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9504bd7686542b8c7bc6f9f778736ee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c43899d7fe4d4be68f",
+    "url": "/static/css/main.04b4bd54.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "f4c43899d7fe4d4be68f",
+    "url": "/static/js/main.bf4bf6f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
